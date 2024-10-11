9d5b7012cd3e5211655bab45b0a2fe8da608e459
Tech-with-anmol <anmollklfh@gmail.com>
2024-10-11 13:15:42 +0530

    working on audio bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({
+          test: /\.(mp3|wav|ogg)$/,
+          type: 'asset/resource',
+        });
+        return config;
+    },
+};
 
 
+  
 
 export default nextConfig;
